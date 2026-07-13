62595e7fd907d934834eda91646242692276cbae
Chris.Drm <jonh00ksksks@gmail.com>
2026-07-13 08:45:18 -0300

    Update Código-V1.0.ino
    Testando algumas coisas novas como uma faixa maior de valores na normalização (10bits) para poder brincar mais com o sensor.
    Retirei a função setLED que não estava mais usando no codigo, quando tudo tiver tudo refinado volto com os leds

--- Código-V1.0.ino
+++ Código-V1.0.ino
@@ -1,7 +1,7 @@
 /*
-  Calibração de BRANCO, PRETO e VERDE - Sensor TCS3200
+  Calibração de BRANCO, PRETO - Sensor TCS3200
   -----------------------------------------------------
-  Versão para Arduino Mega
+  Versão para Arduino Mega 2650
 
   Ligações:
   S0 -> 35
@@ -15,7 +15,7 @@
   Fluxo:
   1. Solicita apontar para o branco
   2. Solicita apontar para o preto
-  3. Faz leitura contínua normalizada (0–255)
+  3. Faz leitura contínua normalizada (0–1023) 
 */
 
 // ------------------ Definição dos pinos ------------------
@@ -62,14 +62,6 @@ void setup() {
   Serial.println("Pinos configurados para Arduino Mega");
   Serial.println("------------------------------------");
 
-  // Configura LEDs
-  pinMode(led1_R, OUTPUT);
-  pinMode(led1_G, OUTPUT);
-  pinMode(led1_B, OUTPUT);
-  pinMode(led2_R, OUTPUT);
-  pinMode(led2_G, OUTPUT);
-  pinMode(led2_B, OUTPUT);
-
   // Botão com pull-up interno
   pinMode(botao, INPUT_PULLUP);
 
@@ -86,41 +78,28 @@ void setup() {
   digitalWrite(S1, LOW);
   delay(500);
 
-  // Começa com os LEDs apagados
-  setLED(led1_R, led1_G, led1_B, LOW, LOW, LOW);
-  setLED(led2_R, led2_G, led2_B, LOW, LOW, LOW);
-
+  // por enquato no refino do sensor de cor não vou usar os leds.
+  
   // ---- Calibração BRANCO ----
   Serial.println("1) Aponte o sensor para o BRANCO e pressione o botao...");
-  setLED(ledR, ledG, ledB, HIGH, HIGH, HIGH);
   esperarBotao();                            // espera o usuário confirmar
   medirCores(R_branco, G_branco, B_branco);  // mede as 3 cores
   mostrarCalibracao("Branco", R_branco, G_branco, B_branco);
 
   // ---- Calibração PRETO ----
   Serial.println("\n2) Aponte o sensor para o PRETO e pressione o botao...");
-  setLED(ledR, ledG, ledB, LOW, LOW, LOW);
   esperarBotao();
   medirCores(R_preto, G_preto, B_preto);
   mostrarCalibracao("Preto", R_preto, G_preto, B_preto);
 
-  // ---- Calibração VERDE ----
-  Serial.println("\n3) Aponte o sensor para o VERDE e pressione o botao...");
-  setLED(ledR, ledG, ledB, LOW, HIGH, LOW);
-  esperarBotao();
-  medirCores(R_verde, G_verde, B_verde);
-  mostrarCalibracao("Verde", R_verde, G_verde, B_verde);
+  /* 
+  Não vou ustilizar mais a calibração do verde pois notei que ele no loop principal nunca era realmente 
+  utilizado. Já tinha notado ano passado mas deixei para revisar esse ano no meu projeto integrador.
+  O fluxo não muda nd, ele apenas le oque e maximo e minimo de claridade e o RGB funciona nesse maximo e
+  minimo, para o sensor saber oque e preto e branco e n ficar ossilando doidamente,
+  */
 
-  // Apaga LED
-  setLED(ledR, ledG, ledB, LOW, LOW, LOW);
-
-  /*// ---- Calibração VERMELHO ----
-  Serial.println("\n4) Aponte o sensor para o VERMELHO e pressione qualquer tecla...");
-  aguardarEntrada();
-  medirCores(R_vermelho, G_vermelho, B_vermelho);
-  mostrarCalibracao("Vermelho", R_vermelho, G_vermelho, B_vermelho);
-
-  Serial.println("\nCalibracao concluida. Iniciando leitura continua...\n");*/
+  Serial.println("\nCalibracao concluida. Iniciando leitura continua...\n");
   delay(500);
 }
 
@@ -133,11 +112,12 @@ void loop() {
   medirCores(R_atual, G_atual, B_atual);
 
   // Normaliza os valores com base nos limites preto e branco
-  // A função map() converte o intervalo [preto, branco] para [0, 255]
+  // A função map() converte o intervalo [preto, branco] para [0, 1023]
   // constrain() garante que o resultado fique dentro dos limites válidos
-  int R_norm = constrain(map(R_atual, R_preto, R_branco, 0, 255), 0, 255);
-  int G_norm = constrain(map(G_atual, G_preto, G_branco, 0, 255), 0, 255);
-  int B_norm = constrain(map(B_atual, B_preto, B_branco, 0, 255), 0, 255);
+  // apenas teste, vou usar de 10bits para ver se consigo uma margem maior para trabalhar
+  int R_norm = constrain(map(R_atual, R_preto, R_branco, 0, 1023), 0, 1023);
+  int G_norm = constrain(map(G_atual, G_preto, G_branco, 0, 1023), 0, 1023);
+  int B_norm = constrain(map(B_atual, B_preto, B_branco, 0, 1023), 0, 1023);
 
   // Exibe as leituras normalizadas (0–255)
   Serial.print("R: ");
@@ -149,36 +129,42 @@ void loop() {
   Serial.print("   -> ");
 
   // Identificação simples de cor (classificação básica)
+  // Pr == provavelmente -> para n travar o arduino
   if (R_norm > 150 && G_norm > 150 && B_norm > 150)
-    Serial.println("Provavelmente BRANCO");
-  /*else if (R_norm > 2*G_norm && R_norm > 2*B_norm)
-    Serial.println("Provavelmente VERMELHO");*/
+    Serial.println("Pr BRANCO");
+  else if (R_norm > 2*G_norm && R_norm > 2*B_norm)
+    Serial.println("Pr VERMELHO");
   else if (G_norm > R_norm && G_norm > 2 * B_norm)
-    Serial.println("Provavelmente VERDE");
-  /*else if (B_norm > R_norm && B_norm > G_norm)
-    Serial.println("Prvavelmente AZUL");*/
+    Serial.println("Pr VERDE");
+  else if (B_norm > R_norm && B_norm > G_norm)
+    Serial.println("Pr AZUL");
   else if (R_norm < 30 && G_norm < 30 && B_norm < 30)
-    Serial.println("Provavelmente PRETO");
+    Serial.println("Pr PRETO");
 
 
 
-
+  /*
   else
     Serial.println("Cor intermediaria ou mista");
-
+  */
   delay(50);  // intervalo entre leituras
 }
 
 
 // =============================================================
 //                      FUNÇÕES AUXILIARES
-// =============================================================
+// =============================================================     
 
-// Aciona LED RGB (nível ativo alto)
-void setLED(int R, int G, int B, bool rVal, bool gVal, bool bVal) {
-  digitalWrite(R, !rVal);
-  digitalWrite(G, !gVal);
-  digitalWrite(B, !bVal);
+// Aguarda o receber INPUT do Serial da IDE
+void esperarBotao() {
+  while (Serial.available() == 0){
+    Seria.read(); // lê oque entra no Serial da IDE
+    }
+  delay(200);
+  while (Serial.available() >= 1){
+    Serial.read();
+  }
+  delay(200);
 }
 
 // --- Mede R, G e B individualmente usando os filtros do TCS3200 ---
@@ -222,15 +208,4 @@ void mostrarCalibracao(const char *nome, unsigned long R, unsigned long G, unsig
   Serial.print(G);
   Serial.print("  B: ");
   Serial.println(B);
-}
-
-
-// Aguarda o botão ser pressionado e solto
-void esperarBotao() {
-  while (digitalRead(botao) == HIGH)
-    ;  // esperando apertar (com INPUT_PULLUP)
-  delay(200);
-  while (digitalRead(botao) == LOW)
-    ;  // esperando soltar
-  delay(200);
 }
